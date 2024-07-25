12931ee2604ade5234b2e155ea558f2e497da6b3
mansourmohamed2608 <47753758+mansourmohamed2608@users.noreply.github.com>
2024-07-26 00:40:42 +0300

    gmadwaaiaaaaal

--- models/User.js
+++ models/User.js
@@ -16,5 +16,3 @@ const UserSchema = new mongoose.Schema({
 
 const User = mongoose.model("User", UserSchema);
 module.exports = User;
-vercel env add GOOGLE_CLIENT_ID
-vercel env add GOOGLE_CLIENT_SECRET
