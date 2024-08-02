075375106aa3f26a83f23e2b080f77d1d7c3e2af
mansourmohamed2608 <47753758+mansourmohamed2608@users.noreply.github.com>
2024-08-02 20:56:01 +0300

    friendrequestss

--- controllers/userController.js
+++ controllers/userController.js
@@ -180,21 +180,20 @@ exports.updateUserDetails = [
  * @route GET /api/users/online-friends
  * @access Private
  */
-exports.getAllFriends = async (req, res) => {
+exports.getOnlineFriends = async (req, res) => {
   try {
     const user = await User.findById(req.user.id).populate("friends");
+    const onlineUsers = getOnlineUsers(); // Get the current state of online users
+    const onlineFriends = user.friends.filter(
+      (friend) => onlineUsers[friend.id]
+    );
 
-    if (!user) {
-      return res.status(404).json({ msg: "User not found" });
-    }
-
-    res.json(user.friends);
+    res.json(onlineFriends);
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server Error");
   }
 };
-
 /**
  * Get User Details
  * @route GET /api/users/details
