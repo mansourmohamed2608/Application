f179ba44a584775e69cddc25a41ff74776c800e6
mansourmohamed2608 <47753758+mansourmohamed2608@users.noreply.github.com>
2024-09-12 11:29:45 +0300

    fixes3

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,7 +1,6 @@
 const Post = require("../models/Post");
 const User = require("../models/User");
 
-
 // Helper function to check if the user has reacted
 const hasUserReacted = (reacts, userId) => {
   for (const [reaction, users] of reacts) {
@@ -102,44 +101,48 @@ exports.getAllPosts = async (req, res) => {
       .limit(parseInt(limit));
 
     const formattedPosts = posts.map((post) => ({
-  _id: post._id,
-  body: post.body,
-  major: post.major,
-  reactsCount: Array.from((post.reacts || new Map()).values()).reduce((a, b) => a + b, 0),
-  hasReacted: hasUserReacted(post.reacts || new Map(), req.user.id),
-  date: timeSince(post.date),
-  user: {
-    name: post.user.name,
-    major: post.user.major,
-    educationLevel: post.user.educationLevel,
-    profilePicture: post.user.profilePicture
-      ? `${req.protocol}://${req.get("host")}/uploads/profilephotos/${post.user.profilePicture}`
-      : null,
-  },
-  comments: post.comments.map((comment) => ({
-    _id: comment._id,
-    body: comment.body,
-    user: comment.user.name,
-    reactsCount: Array.from((comment.reacts || new Map()).values()).reduce(
-      (a, b) => a + b,
-      0
-    ),
-    hasReacted: hasUserReacted(comment.reacts || new Map(), req.user.id),
-    date: timeSince(comment.date),
-    replies: comment.replies.map((reply) => ({
-      _id: reply._id,
-      body: reply.body,
-      user: reply.user.name,
-      reactsCount: Array.from((reply.reacts || new Map()).values()).reduce(
+      _id: post._id,
+      body: post.body,
+      major: post.major,
+      reactsCount: Array.from((post.reacts || new Map()).values()).reduce(
         (a, b) => a + b,
         0
       ),
-      hasReacted: hasUserReacted(reply.reacts || new Map(), req.user.id),
-      date: timeSince(reply.date),
-    })),
-  })),
-}));
-
+      hasReacted: hasUserReacted(post.reacts || new Map(), req.user.id),
+      date: timeSince(post.date),
+      user: {
+        name: post.user.name,
+        major: post.user.major,
+        educationLevel: post.user.educationLevel,
+        profilePicture: post.user.profilePicture
+          ? `${req.protocol}://${req.get("host")}/uploads/profilephotos/${
+              post.user.profilePicture
+            }`
+          : null,
+      },
+      comments: post.comments.map((comment) => ({
+        _id: comment._id,
+        body: comment.body,
+        user: comment.user.name,
+        reactsCount: Array.from((comment.reacts || new Map()).values()).reduce(
+          (a, b) => a + b,
+          0
+        ),
+        hasReacted: hasUserReacted(comment.reacts || new Map(), req.user.id),
+        date: timeSince(comment.date),
+        replies: comment.replies.map((reply) => ({
+          _id: reply._id,
+          body: reply.body,
+          user: reply.user.name,
+          reactsCount: Array.from((reply.reacts || new Map()).values()).reduce(
+            (a, b) => a + b,
+            0
+          ),
+          hasReacted: hasUserReacted(reply.reacts || new Map(), req.user.id),
+          date: timeSince(reply.date),
+        })),
+      })),
+    }));
 
     res.json(formattedPosts);
   } catch (err) {
@@ -147,9 +150,6 @@ exports.getAllPosts = async (req, res) => {
     res.status(500).send("Server Error");
   }
 };
-
-
-
 
 exports.getFriendsPosts = async (req, res) => {
   try {
@@ -249,8 +249,6 @@ exports.getFriendsPosts = async (req, res) => {
   }
 };
 
-
-
 // Get all posts by a specific user ID with pagination
 exports.getPostsByUserId = async (req, res) => {
   const { userId } = req.params;
@@ -322,9 +320,6 @@ exports.getPostsByUserId = async (req, res) => {
     res.status(500).send("Server Error");
   }
 };
-
-
-
 
 // Update a post by ID
 exports.updatePost = async (req, res) => {
@@ -535,7 +530,9 @@ exports.getPostDetailsByPostId = async (req, res) => {
         major: post.user.major,
         educationLevel: post.user.educationLevel,
         profilePicture: post.user.profilePicture
-          ? `${req.protocol}://${req.get("host")}/uploads/profilephotos/${post.user.profilePicture}`
+          ? `${req.protocol}://${req.get("host")}/uploads/profilephotos/${
+              post.user.profilePicture
+            }`
           : null,
       },
       comments: post.comments.map((comment) => ({
@@ -609,6 +606,7 @@ exports.addComment = async (req, res) => {
 exports.addReactionToPost = async (req, res) => {
   const { postId } = req.params;
   const { reaction } = req.body;
+  const userId = req.user.id; // Assuming `req.user.id` contains the logged-in user's ID
 
   if (!reaction) {
     return res.status(400).json({ msg: "Reaction is required" });
@@ -621,8 +619,17 @@ exports.addReactionToPost = async (req, res) => {
       return res.status(404).json({ msg: "Post not found" });
     }
 
-    // Increment the reaction count for the given reaction type
-    post.reacts.set(reaction, (post.reacts.get(reaction) || 0) + 1);
+    // Check if the user has already reacted
+    const currentReactions = post.reacts.get(reaction) || [];
+
+    if (currentReactions.includes(userId)) {
+      return res
+        .status(400)
+        .json({ msg: "User has already reacted with this type" });
+    }
+
+    // Add the user's ID to the array for the given reaction type
+    post.reacts.set(reaction, [...currentReactions, userId]);
 
     await post.save();
 
@@ -637,10 +644,16 @@ exports.addReactionToPost = async (req, res) => {
     res.status(500).send("Server Error");
   }
 };
+
 // Remove a reaction from a post
 exports.removeReactionFromPost = async (req, res) => {
   const { postId } = req.params;
+  const { reaction } = req.body;
   const userId = req.user.id;
+
+  if (!reaction) {
+    return res.status(400).json({ msg: "Reaction is required" });
+  }
 
   try {
     const post = await Post.findById(postId);
@@ -649,25 +662,24 @@ exports.removeReactionFromPost = async (req, res) => {
       return res.status(404).json({ msg: "Post not found" });
     }
 
-    let removed = false;
-    for (const [reactionType, userIds] of post.reacts) {
-      if (userIds.includes(userId)) {
-        post.reacts.set(
-          reactionType,
-          userIds.filter((id) => id.toString() !== userId.toString())
-        );
-        removed = true;
-        break;
-      }
-    }
+    // Get the current reactions for the given type
+    let currentReactions = post.reacts.get(reaction) || [];
 
-    if (!removed) {
-      return res.status(404).json({ msg: "User has not reacted" });
+    // Remove the user's ID from the array if it exists
+    currentReactions = currentReactions.filter(
+      (id) => id.toString() !== userId
+    );
+
+    // If the array is now empty, remove the reaction type from the map
+    if (currentReactions.length === 0) {
+      post.reacts.delete(reaction);
+    } else {
+      post.reacts.set(reaction, currentReactions);
     }
 
     await post.save();
 
-    res.json({ msg: "Reaction removed", post });
+    res.json({ msg: "Reaction removed" });
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server Error");
